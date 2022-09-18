20a14eda60efbc660db9cb0b3b419e6895dc611a
Hcabrejas <101750877+Hcabrejas@users.noreply.github.com>
2022-09-18 18:36:01 +0200

    Add files via upload

---
+++ 01-app.js
@@ -0,0 +1,40 @@
+//Inicializar una variable con un valor
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+
+//Las variables se puede reasginar
+producto = 'Monitor de 10 pulgadas';
+console.log(producto);
+
+//Javascript es un lenguaje de tipo dinamico, no es espceifica tipo de dato
+producto = 20;
+console.log(producto);
+
+
+//Se puede inicializar sin valor y asignarlo despues
+var disponible;
+disponible = true;
+
+disponible = false;
+
+// Inicializar mñultiples variables.
+var categoria = ' Computadoras',
+ marca = "Marca Famosa",
+ calificacion = 5;
+
+//  // No puedo inicializar variables con numeros
+//  var 99dias;
+//  var dias99;
+
+// var 01_;
+// var _01;
+// var _usuario;
+// var __usuario;
+
+
+// //CamelCase
+// var nombreProductoCategoriaPrecio;
+// //Snake-Underscore
+// var nombre_producto_categoria_precio;
+// //PascalCase
+// var Producto;
